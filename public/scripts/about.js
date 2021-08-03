841d0451545fdf59d310fe7a186ae179b21c6929
Kayci Parcells <kayci@Kaycis-MBP.attlocal.net>
2021-08-03 11:52:04 -0500

    more testing

--- public/scripts/about.js
+++ public/scripts/about.js
@@ -4,19 +4,11 @@ var firestore = firebase.firestore();
 var storage = firebase.storage();
 var pathReference = storage.ref("flamelink/media/Composition-01.jpg")
 
-//const docRef = firestore.collection("fl_content").doc("fXJvFXnibGxl7maesHAr");
 const domAboutImage = document.querySelector("#aboutImage");
 
 window.onload = function () {
     document.getElementById("first-heading").innerHTML = "Kayci Parcells";
     console.log("hello world");
-    
-//    docRef.get().then(function (doc) {
-//        if (doc && doc.exists) {
-//            const docData = doc.data();
-//            domAboutImage.src = docData.aboutImage;
-//            console.log("data retrieved: ", docData.aboutImage);
-//        }
     var url = pathReference.getDownloadURL();
     domAboutImage.setAttribute('src', url);
     }).catch(function (error){
