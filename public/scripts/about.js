055c0c823cb429b1a44a52d1e2df418668e07231
Kayci Parcells <kayci@Kaycis-MBP.attlocal.net>
2021-08-03 11:44:47 -0500

    trying to grab image from storage

--- public/scripts/about.js
+++ public/scripts/about.js
@@ -1,20 +1,24 @@
 "use strict";
 
 var firestore = firebase.firestore();
+var storage = firebase.storage();
+var pathReference = storage.ref("media/Composition-01.jpg")
 
-const docRef = firestore.collection("fl_content").doc("fXJvFXnibGxl7maesHAr");
+//const docRef = firestore.collection("fl_content").doc("fXJvFXnibGxl7maesHAr");
 const domAboutImage = document.querySelector("#aboutImage");
 
 window.onload = function () {
-    document.getElementById("first-heading").innerHTML = "JS Connected2";
+    document.getElementById("first-heading").innerHTML = "Kayci Parcells";
     console.log("hello world");
     
-    docRef.get().then(function (doc) {
-        if (doc && doc.exists) {
-            const docData = doc.data();
-            domAboutImage.src = docData.aboutImage;
-            console.log("data retrieved: ", docData.aboutImage);
-        }
+//    docRef.get().then(function (doc) {
+//        if (doc && doc.exists) {
+//            const docData = doc.data();
+//            domAboutImage.src = docData.aboutImage;
+//            console.log("data retrieved: ", docData.aboutImage);
+//        }
+    var url = pathReference.getDownloadURL();
+    domAboutImage.setAttribute('src', url);
     }).catch(function (error){
         console.log("error in aboutImage: ", error);
     })
