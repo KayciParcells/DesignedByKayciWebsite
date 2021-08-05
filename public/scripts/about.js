a1991e1ce4c0e5884e8b948d39f0770e7fa184b8
Kayci Parcells <kayci@Kaycis-MBP.attlocal.net>
2021-08-05 12:33:36 -0500

    questioning my life choices

--- public/scripts/about.js
+++ public/scripts/about.js
@@ -1,9 +1,7 @@
-import firebase from 'firebase/app';
-import 'firebase/storage';
+var firebase = require('firebase/app');
+require('firebase/storage');
 
 var storage = firebase.storage();
-var pathReference = storage.ref("flamelink/media/Composition-01.jpg");
-var domAboutImage = document.querySelector("#aboutImage");
 var storageRef = firebase.storage.ref("about/Composition_ForScreens.png");
 
 window.onload = function () {
