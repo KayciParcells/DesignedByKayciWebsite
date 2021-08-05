b7dd4c5b1a4d988691cfc17f0b57a6328f35e412
Kayci Parcells <kayci@Kaycis-MBP.attlocal.net>
2021-08-05 15:28:09 -0500

    I think this will blow up

---
+++ public/scripts/index.js
@@ -0,0 +1,11 @@
+import firebase from "firebase/app";
+import "firebase/storage";
+
+var storage = firebase.storage();
+var storageRef = storage.ref("about/Composition_ForScreens.png");
+
+window.onload = function () {
+        storageRef.getDownloadURL().then(function(url) {
+          console.log(url);
+        });
+    };
