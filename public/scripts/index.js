0e15cac98a1127085f996ed00cf919fdc15b3442
Kayci Parcells <kayci@Kaycis-MBP.attlocal.net>
2021-08-05 15:38:44 -0500

    trying to replace source

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,8 +1,10 @@
 var storage = firebase.storage();
 var storageRef = storage.ref("about/Composition_ForScreens.png");
+var domAboutImage = document.getElementById("#aboutImage");
 
 window.onload = function () {
-        storageRef.getDownloadURL().then(function(url) {
-          console.log(url);
-        });
-    };
+    storageRef.getDownloadURL().then(function(url) {
+    console.log(url);
+    domAboutImage.src = url;
+    });
+};
