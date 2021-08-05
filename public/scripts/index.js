66a972b04c0c35c799abe45a7a08a0d3d53c0aa6
Kayci Parcells <kayci@Kaycis-MBP.attlocal.net>
2021-08-05 15:47:15 -0500

    might help if I selected the object

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,6 +1,6 @@
 var storage = firebase.storage();
 var storageRef = storage.ref("about/Composition_ForScreens.png");
-var domAboutImage = document.getElementById("#aboutImage");
+var domAboutImage = document.querySelector("#aboutImage");
 
 window.onload = function () {
     storageRef.getDownloadURL().then(function(url) {
